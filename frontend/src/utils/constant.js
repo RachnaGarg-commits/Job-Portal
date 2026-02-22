2c43b6f8ab7eb73dfb4a9ca2a78cc4bea4a056f3
RachnaGarg-commits <rachnagarg215@gmail.com>
2026-02-22 14:38:10 +0530

    updated API

--- frontend/src/utils/constant.js
+++
@@ -1,4 +0,0 @@
-
-export const JOB_API_END_POINT="https://jobportal-backend-8wn5.onrender.com/api/v1/job";
-export const APPLICATION_API_END_POINT="https://jobportal-backend-8wn5.onrender.com/api/v1/application";
-export const COMPANY_API_END_POINT="https://jobportal-backend-8wn5.onrender.com/api/v1/company";
